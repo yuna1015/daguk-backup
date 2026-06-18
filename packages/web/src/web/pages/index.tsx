f2aedf6b91aff900d144cc7a79ffffe834293bdb
Daguk Textile <daguk@daguk-textile.com>
2026-06-18 02:52:23 +0000

    fix: 사업자정보 정렬 맞춤, Products→Fabric Library 타이틀 변경

--- packages/web/src/web/pages/index.tsx
+++ packages/web/src/web/pages/index.tsx
@@ -186,10 +186,10 @@ function Products() {
         <div className="dg-eyebrow reveal">
           <span className="ln" />
           <span>Products</span>
-          <span className="sub">Knitting · Greige & Finishing</span>
+          <span className="sub">Fabric Library</span>
         </div>
         <h2 className="dg-big reveal" style={{ marginTop: 24, marginBottom: 0 }}>
-          Products
+          Fabric Library
         </h2>
 
         <div className="dg-prod-list">
@@ -277,7 +277,7 @@ function Footer() {
           <p>경기도 포천시 가산면 메나리길 29</p>
         </div>
       </div>
-      <div className="dg-bizinfo" style={{ borderTop: "1px solid rgba(255,255,255,0.1)", marginTop: 32, paddingTop: 20, fontSize: 12, opacity: 0.5, lineHeight: 2 }}>
+      <div style={{ maxWidth: 1180, margin: "0 auto", borderTop: "1px solid rgba(255,255,255,0.1)", marginTop: 32, paddingTop: 20, fontSize: 12, opacity: 0.5, lineHeight: 2 }}>
         <p>상호명: 다국텍스타일 &nbsp;|&nbsp; 대표자: 이창업 &nbsp;|&nbsp; 사업자등록번호: 207-72-62958</p>
         <p>주소: 경기도 포천시 가산면 메나리길 29 &nbsp;|&nbsp; TEL: 031-543-6782 &nbsp;|&nbsp; FAX: 031-543-6792 &nbsp;|&nbsp; HP: 010-3213-1828</p>
       </div>
